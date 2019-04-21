33493b1bfaeda91e42372a7c19f4fe733c61de8a
zhongchun159@sina.com <zhongchun159@sina.com>
2019-04-21 18:09:02 +0800

    初始化

---
+++ src/main/java/cn/zcp/demo/zookeeper/base/ZookeeperBase.java
@@ -0,0 +1,15 @@
+package cn.zcp.demo.zookeeper.base;
+
+/**
+ * @author zhongchunping
+ * @version 1.0
+ * @Time 2019-04-21 16:52
+ * @describe demo-parent 使用原生
+ */
+public class ZookeeperBase {
+
+    public static void main(String[] args) {
+        System.out.println("11111111111111");
+    }
+
+}
